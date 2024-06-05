8233244d147ac0483536dfa828fb0e452571db9e
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-06-05 23:46:54 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-commoncurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+
+docker pull analoglabs/timechain
+read -p "请输入节点名字: " your_node_name
+node_name="$your_node_name"
+docker run -d -p 9944:9944 -p 30303:30303 --name analog analoglabs/timechain \
+  --base-path /data \
+  --rpc-external \
+  --unsafe-rpc-external \
+  --rpc-cors all \
+  --name="$node_name" \
+  --rpc-methods Unsafe
